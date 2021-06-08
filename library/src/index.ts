ab99c10dfb726fd9e16ae91c2794251556b236ae
Bilal Uslu <bilal@bilaluslu.com>
2021-06-08 18:19:07 +0100

    init38

--- library/src/index.ts
+++ library/src/index.ts
@@ -1,6 +1,6 @@
 export const sum = (a: number, b: number) => {
   if ('development' === process.env.NODE_ENV) {
-    console.log('boopxxxxx');
+    console.log('boopxxxxx1');
   }
   return a + b;
 };
